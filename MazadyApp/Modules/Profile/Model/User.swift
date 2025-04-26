5be89d9820f47248689380daef6cd1cdbf5f8e8e
wafaa <w.farag@speedi.sa>
2025-04-26 12:41:26 +0300

    constraints

--- MazadyApp/Modules/Profile/Model/User.swift
+++ MazadyApp/Modules/Profile/Model/User.swift
@@ -10,13 +10,21 @@ import Foundation
 struct User: Codable {
     let id: Int
     let name: String
-    let email: String?
-    let profileImage: String?
-    
+    let image: String?
+    let userName: String
+    let followingCount: Int
+    let followersCount: Int
+    let countryName: String
+    let cityName: String
+
     enum CodingKeys: String, CodingKey {
         case id
         case name
-        case email
-        case profileImage = "profile_image"
+        case image
+        case userName = "user_name"
+        case followingCount = "following_count"
+        case followersCount = "followers_count"
+        case countryName = "country_name"
+        case cityName = "city_name"
     }
 }
--- MazadyApp/Modules/Profile/View/ProfileViewController.swift
+++ MazadyApp/Modules/Profile/View/ProfileViewController.swift
@@ -14,8 +14,11 @@ class ProfileViewController: BaseViewController {
     //@IBOutlet weak var profileTabsView: Segmentio!
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var emailLabel: UILabel!
-    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var followersCountLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var followingCountLabel: UILabel!
     // MARK: - Properties
     var viewModel: ProfileViewModel! // injected later
     private let disposeBag = DisposeBag()
@@ -39,9 +42,12 @@ class ProfileViewController: BaseViewController {
             .asDriver()
             .drive(onNext: { [weak self] user in
                 guard let self = self, let user = user else { return }
-                self.nameLabel.text = user.name
-                self.emailLabel.text = user.email
-                if let imageUrl = user.profileImage, let url = URL(string: imageUrl) {
+                nameLabel.text = user.name
+                userNameLabel.text = user.userName
+                locationLabel.text = "\(user.countryName), \(user.cityName )"
+                followersCountLabel.text = "\(user.followersCount)"
+                followingCountLabel.text = "\(user.followingCount)"
+                if let imageUrl = user.image, let url = URL(string: imageUrl) {
                     DispatchQueue.global().async {
                         if let data = try? Data(contentsOf: url) {
                             DispatchQueue.main.async {
