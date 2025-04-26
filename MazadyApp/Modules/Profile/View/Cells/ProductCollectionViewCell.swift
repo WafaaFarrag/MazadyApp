a84cdf22711bcb6e46223d3e9f69c21d329fed53
wafaa <w.farag@speedi.sa>
2025-04-26 15:40:26 +0300

    design cell

---
+++ MazadyApp/Modules/Profile/View/Cells/ProductCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCollectionViewCell.swift
+//  MazadyApp
+//
+//  Created by wafaa farrag on 26/04/2025.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
