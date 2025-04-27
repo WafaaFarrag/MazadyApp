ef9c3a06328f230d021d103e0e1895d827b60371
wafaa <w.farag@speedi.sa>
2025-04-27 21:53:10 +0300

    ui

--- MazadyApp/Modules/Profile/View/Cells/ProductCollectionViewCell.swift
+++ MazadyApp/Modules/Profile/View/Cells/ProductCollectionViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 import UIKit
 
 class ProductCollectionViewCell: UICollectionViewCell {
-
+    
     @IBOutlet weak var oldPriceLabel: UILabel!
     @IBOutlet weak var productImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
@@ -24,12 +24,12 @@ class ProductCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var hoursLabel: UILabel!
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var bottomSpacerView: UIView!
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setupUI()
     }
-
+    
     override func prepareForReuse() {
         super.prepareForReuse()
         daysLabel.text = ""
@@ -40,32 +40,32 @@ class ProductCollectionViewCell: UICollectionViewCell {
         offerPriceLabel.attributedText = nil
         oldPriceLabel.attributedText = nil
         productImageView.image = nil
-
+        
         timerStackView.isHidden = true
         containerOfferPriceStackView.isHidden = true
     }
-
+    
     private func setupUI() {
         layer.cornerRadius = 20
         clipsToBounds = true
-
+        
         productImageView.contentMode = .scaleAspectFill
         productImageView.layer.cornerRadius = 12
         productImageView.clipsToBounds = true
-
+        
         offerPriceLabel.textColor = .red
         titleLabel.numberOfLines = 0
         titleLabel.lineBreakMode = .byWordWrapping
-
+        
         containerOfferPriceStackView.setContentHuggingPriority(.required, for: .vertical)
         containerOfferPriceStackView.isHidden = true
-
+        
         timerStackView.setContentHuggingPriority(.required, for: .vertical)
         timerStackView.isHidden = true
-
+        
         bottomSpacerView.setContentHuggingPriority(.defaultLow, for: .vertical)
     }
-
+    
     func configure(with product: Product) {
         if let url = URL(string: product.image) {
             productImageView.load(url: url) { [weak self] in
@@ -77,10 +77,10 @@ class ProductCollectionViewCell: UICollectionViewCell {
             productImageView.layer.cornerRadius = 20
             productImageView.clipsToBounds = true
         }
-
+        
         titleLabel.text = product.name
         priceLabel.text = "\(product.price) \(product.currency)"
-
+        
         if let offer = product.offer {
             containerOfferPriceStackView.isHidden = false
             let attributeString = NSAttributedString(
@@ -95,7 +95,7 @@ class ProductCollectionViewCell: UICollectionViewCell {
         } else {
             containerOfferPriceStackView.isHidden = true
         }
-
+        
         if let secondsLeft = product.endDate {
             timerStackView.isHidden = false
             updateCountdown(secondsLeft: secondsLeft)
@@ -103,13 +103,13 @@ class ProductCollectionViewCell: UICollectionViewCell {
             timerStackView.isHidden = true
         }
     }
-
+    
     func updateCountdown(secondsLeft: Double) {
         let totalSeconds = Int(secondsLeft)
         let days = totalSeconds / (24 * 3600)
         let hours = (totalSeconds % (24 * 3600)) / 3600
         let minutes = (totalSeconds % 3600) / 60
-
+        
         daysLabel.text = "\(days)"
         hoursLabel.text = "\(hours)"
         minutesLabel.text = "\(minutes)"
