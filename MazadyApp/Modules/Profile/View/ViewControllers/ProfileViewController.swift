415cbd6fa898329eae95a19b4fa76491af8a9402
wafaa <w.farag@speedi.sa>
2025-04-27 06:40:50 +0300

    logic for add top Tabs

--- MazadyApp/Modules/Profile/View/ViewControllers/ProfileViewController.swift
+++ MazadyApp/Modules/Profile/View/ViewControllers/ProfileViewController.swift
@@ -1,371 +1,3 @@
-////
-////  ProfileViewController.swift
-////  MazadyApp
-////
-////  Created by wafaa farrag on 25/04/2025.
-////
-//
-//import Foundation
-//import UIKit
-//import RxSwift
-//import RxDataSources
-//
-//class ProfileViewController: BaseViewController {
-//    // MARK: - Outlets
-//    //@IBOutlet weak var profileTabsView: Segmentio!
-//    @IBOutlet weak var containerView: UIView!
-//    @IBOutlet weak var searchTextField: UITextField!
-//    @IBOutlet weak var profileImageView: UIImageView!
-//    @IBOutlet weak var nameLabel: UILabel!
-//    @IBOutlet weak var userNameLabel: UILabel!
-//    @IBOutlet weak var locationLabel: UILabel!
-//    @IBOutlet weak var followersCountLabel: UILabel!
-//    @IBOutlet weak var languageLabel: UILabel!
-//    @IBOutlet weak var followingCountLabel: UILabel!
-//    @IBOutlet weak var containerCollectionView: UICollectionView!
-//    @IBOutlet weak var productsButton: UIButton!
-//    @IBOutlet weak var reviewsButton: UIButton!
-//    @IBOutlet weak var followersButton: UIButton!
-//    @IBOutlet weak var underlineView: UIView!
-//    @IBOutlet weak var underlineLeadingConstraint: NSLayoutConstraint!
-//
-//    
-//    @IBAction func searchBtnAction(_ sender: Any) {
-//        guard let keyword = searchTextField.text, !keyword.isEmpty else {
-//            return
-//        }
-//        
-//        viewModel.searchProducts(by: keyword)
-//        searchTextField.resignFirstResponder()
-//        
-//    }
-//    
-//    @IBAction func tabButtonTapped(_ sender: UIButton) {
-//        let newTabIndex = sender.tag
-//        if selectedTabIndex != newTabIndex {
-//            displayCurrentTab(newTabIndex)
-//            updateTabsUI(to: newTabIndex)
-//            selectedTabIndex = newTabIndex
-//        }
-//    }
-//
-//    
-//    // MARK: - Properties
-//    var viewModel: ProfileViewModel! // injected later
-//    private let disposeBag = DisposeBag()
-//    private var dataSource: RxCollectionViewSectionedReloadDataSource<ProfileSectionModel>!
-//    private var selectedTabIndex = 0
-//    private var currentChildViewController: UIViewController?
-//    
-//    // MARK: - Lifecycle
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        viewModel.loadAllData()
-//        setupCollectionView()
-//        setupView()
-//        bindViewModel()
-//        
-//    }
-//    
-//    // MARK: - Setup
-//    func configure(with viewModel: ProfileViewModel) {
-//        self.viewModel = viewModel
-//    }
-//    
-//    private func setupView() {
-//        searchTextField.delegate = self
-//        setupSearchFieldListener()
-//        displayCurrentTab(0)
-//        updateTabsUI(to: 0)
-//    }
-//    private func setupCollectionView() {
-//        containerCollectionView.delegate = self
-//        // 1. Register cells
-//        containerCollectionView.register(UINib(nibName: "ProductCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "ProductCollectionViewCell")
-//        containerCollectionView.register(UINib(nibName: "AdvertisementCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "AdvertisementCollectionViewCell")
-//        containerCollectionView.register(UINib(nibName: "TagsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "TagsCollectionViewCell")
-//        containerCollectionView.register(UINib(nibName: "TagsHeaderView", bundle: nil),forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "TagsHeaderView")
-//
-//        // 2. Setup Data Source
-//        dataSource = RxCollectionViewSectionedReloadDataSource<ProfileSectionModel>(
-//            configureCell: { [weak self] dataSource, collectionView, indexPath, item in
-//                guard self != nil else { return UICollectionViewCell() }
-//                let section = dataSource.sectionModels[indexPath.section]
-//                
-//                switch section {
-//                case .productsSection(let products):
-//                    let product = products[indexPath.item]
-//                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCollectionViewCell", for: indexPath) as! ProductCollectionViewCell
-//                    cell.configure(with: product)
-//                    return cell
-//
-//                case .adsSection(let ads):
-//                    let ad = ads[indexPath.item]
-//                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AdvertisementCollectionViewCell", for: indexPath) as! AdvertisementCollectionViewCell
-//                    cell.configure(with: ad)
-//                    return cell
-//
-//                case .tagsSection(let tags):
-//                    let tag = tags[indexPath.item]
-//                    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TagsCollectionViewCell", for: indexPath) as! TagsCollectionViewCell
-//                    cell.configure(with: tag)
-//                    return cell
-//                }
-//            },
-//            
-//            configureSupplementaryView: { dataSource, collectionView, kind, indexPath in
-//                if kind == UICollectionView.elementKindSectionHeader {
-//                    let section = dataSource.sectionModels[indexPath.section]
-//                    switch section {
-//                    case .tagsSection:
-//                        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TagsHeaderView", for: indexPath) as! TagsHeaderView
-//                        header.titleLabel.text = "Top Tags"
-//                        return header
-//                    default:
-//                        return UICollectionReusableView()
-//                    }
-//                } else {
-//                    return UICollectionReusableView()
-//                }
-//            }
-//        )
-//
-//        containerCollectionView.collectionViewLayout = createLayout()
-//    }
-//    
-//    private func createLayout() -> UICollectionViewLayout {
-//        return UICollectionViewCompositionalLayout { [weak self] sectionIndex, environment in
-//            guard let self = self else { return nil }
-//            let section = self.dataSource.sectionModels[sectionIndex]
-//            
-//            switch section {
-//            case .productsSection:
-//                let itemsPerRow: CGFloat = 3
-//                let columnSpacing: CGFloat = 8
-//                
-//                // Item
-//                let itemSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0 / itemsPerRow),
-//                    heightDimension: .estimated(200)
-//                )
-//                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-//                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: columnSpacing / 2, bottom: 0, trailing: columnSpacing / 2)
-//                
-//                
-//                // Group
-//                let groupItemSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .estimated(200)
-//                )
-//                let group = NSCollectionLayoutGroup.horizontal(
-//                    layoutSize: groupItemSize,
-//                    subitems: [item]
-//                )
-//                group.interItemSpacing = .fixed(0)
-//                
-//                // Section
-//                let section = NSCollectionLayoutSection(group: group)
-//                section.interGroupSpacing = 8
-//                section.contentInsets = .zero
-//                
-//                return section
-//                
-//            case .adsSection:
-//                let itemSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .absolute(140)
-//                )
-//                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-//                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
-//                
-//                let groupSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .absolute(140)
-//                )
-//                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-//                
-//                let section = NSCollectionLayoutSection(group: group)
-//                section.contentInsets = NSDirectionalEdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0)
-//                section.interGroupSpacing = 8
-//                
-//                return section
-//                
-//            case .tagsSection:
-//                let itemSize = NSCollectionLayoutSize(
-//                    widthDimension: .estimated(100),
-//                    heightDimension: .absolute(40)
-//                )
-//                let item = NSCollectionLayoutItem(layoutSize: itemSize)
-//                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0) // << FIXED
-//
-//                let groupSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .estimated(200)
-//                )
-//                
-//                let group = NSCollectionLayoutGroup.horizontal(
-//                    layoutSize: groupSize,
-//                    subitems: [item]
-//                )
-//                group.interItemSpacing = .fixed(8)
-//
-//                let section = NSCollectionLayoutSection(group: group)
-//                section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16)
-//                section.interGroupSpacing = 8
-//                section.orthogonalScrollingBehavior = .none
-//                
-//                // --- ADD HEADER ---
-//                let headerSize = NSCollectionLayoutSize(
-//                    widthDimension: .fractionalWidth(1.0),
-//                    heightDimension: .absolute(44)
-//                )
-//                let header = NSCollectionLayoutBoundarySupplementaryItem(
-//                    layoutSize: headerSize,
-//                    elementKind: UICollectionView.elementKindSectionHeader,
-//                    alignment: .top
-//                )
-//                section.boundarySupplementaryItems = [header]
-//
-//                return section
-//            }
-//        }
-//    }
-//    
-//    // MARK: - Binding
-//    private func bindViewModel() {
-//        viewModel.user
-//            .asDriver()
-//            .drive(onNext: { [weak self] user in
-//                guard let self = self, let user = user else { return }
-//                nameLabel.text = user.name
-//                userNameLabel.text = user.userName
-//                locationLabel.text = "\(user.countryName), \(user.cityName )"
-//                followersCountLabel.text = "\(user.followersCount)"
-//                followingCountLabel.text = "\(user.followingCount)"
-//                if let imageUrl = user.image, let url = URL(string: imageUrl) {
-//                    DispatchQueue.global().async {
-//                        if let data = try? Data(contentsOf: url) {
-//                            DispatchQueue.main.async {
-//                                self.profileImageView.image = UIImage(data: data)
-//                            }
-//                        }
-//                    }
-//                }
-//            })
-//            .disposed(by: disposeBag)
-//        
-//        Observable.combineLatest(viewModel.products, viewModel.ads, viewModel.tags)
-//            .map { products, ads, tags -> [ProfileSectionModel] in
-//                return [
-//                    .productsSection(items: products),
-//                    .adsSection(items: ads),
-//                    .tagsSection(items: tags)
-//                ]
-//            }
-//            .bind(to: containerCollectionView.rx.items(dataSource: dataSource))
-//            .disposed(by: disposeBag)
-//    }
-//}
-//
-//extension ProfileViewController: UITextFieldDelegate {
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        guard let keyword = textField.text, !keyword.isEmpty else { return true }
-//        
-//        viewModel.searchProducts(by: keyword)
-//        textField.resignFirstResponder()
-//        return true
-//    }
-//    
-//    private func setupSearchFieldListener() {
-//        searchTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
-//    }
-//
-//    @objc private func textFieldDidChange(_ textField: UITextField) {
-//        if let text = textField.text, text.isEmpty {
-//            viewModel.loadProducts()
-//        }
-//    }
-//
-//}
-//
-//extension ProfileViewController: UIScrollViewDelegate, UICollectionViewDelegate {
-//    private func displayCurrentTab(_ tabIndex: Int) {
-//        let newViewController: UIViewController
-//        switch tabIndex {
-//        case 0:
-//            newViewController = instantiateViewController(named: "ProductsViewController")
-//        case 1:
-//            newViewController = instantiateViewController(named: "ReviewsViewController")
-//        case 2:
-//            newViewController = instantiateViewController(named: "FollowersViewController")
-//        default:
-//            return
-//        }
-//        
-//        guard let currentVC = currentChildViewController else {
-//            // First time, no animation needed
-//            addChild(newViewController)
-//            containerView.addSubview(newViewController.view)
-//            newViewController.view.frame = containerView.bounds
-//            newViewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//            newViewController.didMove(toParent: self)
-//            currentChildViewController = newViewController
-//            return
-//        }
-//        
-//        // Decide slide direction
-//        let isForward = tabIndex > selectedTabIndex
-//        
-//        addChild(newViewController)
-//        newViewController.view.frame = containerView.bounds.offsetBy(dx: isForward ? containerView.frame.width : -containerView.frame.width, dy: 0)
-//        containerView.addSubview(newViewController.view)
-//        
-//        UIView.animate(withDuration: 0.3, animations: {
-//            newViewController.view.frame = self.containerView.bounds
-//            currentVC.view.frame = self.containerView.bounds.offsetBy(dx: isForward ? -self.containerView.frame.width : self.containerView.frame.width, dy: 0)
-//        }, completion: { _ in
-//            currentVC.willMove(toParent: nil)
-//            currentVC.view.removeFromSuperview()
-//            currentVC.removeFromParent()
-//            
-//            newViewController.didMove(toParent: self)
-//            self.currentChildViewController = newViewController
-//        })
-//    }
-//
-//    
-//    private func updateTabsUI(to tabIndex: Int) {
-//        [productsButton, reviewsButton, followersButton].enumerated().forEach { index, button in
-//            button.setTitleColor(index == tabIndex ? .systemPink : .gray, for: .normal)
-//        }
-//
-//        let buttonWidth = view.frame.width / 3
-//        underlineLeadingConstraint.constant = CGFloat(tabIndex) * buttonWidth
-//        
-//        UIView.animate(withDuration: 0.3) {
-//            self.view.layoutIfNeeded()
-//        }
-//    }
-//
-//    
-//    private func scrollToSelectedTab() {
-//        let indexPath = IndexPath(item: selectedTabIndex, section: 0)
-//        containerCollectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
-//    }
-//
-//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-//        selectedTabIndex = Int(scrollView.contentOffset.x / scrollView.frame.width)
-//       // updateTabsUI(animated: true)
-//    }
-//
-//    private func instantiateViewController(named name: String) -> UIViewController {
-//        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
-//    }
-//
-//
-//}
-//
-
 
 //
 //  ProfileViewController.swift
@@ -390,13 +22,12 @@ class ProfileViewController: BaseViewController {
     @IBOutlet weak var followersCountLabel: UILabel!
     @IBOutlet weak var languageLabel: UILabel!
     @IBOutlet weak var followingCountLabel: UILabel!
-    
     @IBOutlet weak var productsButton: UIButton!
     @IBOutlet weak var reviewsButton: UIButton!
     @IBOutlet weak var followersButton: UIButton!
     @IBOutlet weak var underlineView: UIView!
     @IBOutlet weak var underlineLeadingConstraint: NSLayoutConstraint!
-
+    
     // MARK: - Actions
     @IBAction func searchBtnAction(_ sender: Any) {
         guard let keyword = searchTextField.text, !keyword.isEmpty else { return }
@@ -418,7 +49,7 @@ class ProfileViewController: BaseViewController {
     private let disposeBag = DisposeBag()
     private var selectedTabIndex = 0
     private var currentChildViewController: UIViewController?
-
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -426,7 +57,7 @@ class ProfileViewController: BaseViewController {
         bindViewModel()
         viewModel.loadAllData()
     }
-
+    
     // MARK: - Setup
     func configure(with viewModel: ProfileViewModel) {
         self.viewModel = viewModel
@@ -444,7 +75,9 @@ class ProfileViewController: BaseViewController {
         let newViewController: UIViewController
         switch tabIndex {
         case 0:
-            newViewController = instantiateViewController(named: "ProductsViewController")
+            let productsVC = instantiateViewController(named: "ProductsViewController") as! ProductsViewController
+            productsVC.viewModel = self.viewModel
+            newViewController = productsVC
         case 1:
             newViewController = instantiateViewController(named: "ReviewsViewController")
         case 2:
@@ -495,7 +128,7 @@ class ProfileViewController: BaseViewController {
             self.view.layoutIfNeeded()
         }
     }
-
+    
     private func instantiateViewController(named name: String) -> UIViewController {
         return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: name)
     }
@@ -538,7 +171,7 @@ extension ProfileViewController: UITextFieldDelegate {
     private func setupSearchFieldListener() {
         searchTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
     }
-
+    
     @objc private func textFieldDidChange(_ textField: UITextField) {
         if let text = textField.text, text.isEmpty {
             viewModel.loadProducts()
