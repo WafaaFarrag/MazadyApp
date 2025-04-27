3b19d6e6ff0a7c3c40ba2bc8c39b53653bf15260
wafaa <w.farag@speedi.sa>
2025-04-27 22:04:02 +0300

    localization

--- MazadyApp/Base/Utilities/SwiftMessagesService.swift
+++ MazadyApp/Base/Utilities/SwiftMessagesService.swift
@@ -13,7 +13,7 @@ class SwiftMessagesService {
     static func show(message: String, theme: Theme = .error) {
         let view = MessageView.viewFromNib(layout: .messageView)
         view.configureTheme(theme)
-        view.configureContent(title: "", body: message)
+        view.configureContent(title: message, body: "")
         SwiftMessages.show(view: view)
     }
 }
